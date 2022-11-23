ad3838fb216381633568b4938c1ef3bad878128e
bernardo <beefreguglia@gmail.com~>
2022-11-23 18:20:35 -0300

    Create card component

---
+++ src/components/Card/styles.ts
@@ -0,0 +1,38 @@
+import styled from 'styled-components'
+
+export const CardContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 2rem;
+  flex-direction: column;
+  background: ${({ theme }) => theme['blue-750']};
+  border-radius: 10px;
+  cursor: pointer;
+  border: 2px solid transparent;
+  transition: 300ms;
+
+  &:hover {
+    border: 2px solid ${({ theme }) => theme['blue-600']};
+  }
+`
+
+export const TitleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 1rem;
+  margin-bottom: 1.25rem;
+
+  h1 {
+    font-size: 1.25rem;
+    color: ${({ theme }) => theme['blue-100']};
+    overflow-wrap: break-word;
+    max-width: 75%;
+  }
+
+  p {
+    font-size: 0.875rem;
+    color: ${({ theme }) => theme['blue-450']};
+  }
+`
